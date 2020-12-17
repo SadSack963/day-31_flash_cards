476d623d04d79f141d14bbe834a3ec75701d0992
john <john@jpatmore.plus.com>
2020-12-17 22:00:56 +0000

    Initial code

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+WINDOW_SIZE = (825, 550)
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = tk.Tk()
+window.title("German Flash Cards")
+window.minsize(800, 526)
+
+
+
+# -----------------------------------
+
+window.mainloop()
