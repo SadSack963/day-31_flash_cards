3ac296f5bbb9608690bbbca65d7f5129f8c46060
john <john@jpatmore.plus.com>
2020-12-18 00:17:55 +0000

    Front card layout done, Window background color

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ word_font = ("Arial", 48, "bold")
 
 window = tk.Tk()
 window.title("German Flash Cards")
-window.minsize(800, 526)
+window.minsize(800, 650)
+window.configure(background=BACKGROUND_COLOR)
 
 frame_card = tk.Frame(window, bg=BACKGROUND_COLOR, bd=0)
 canvas_card = tk.Canvas(frame_card, bg=BACKGROUND_COLOR, height=CANVAS_HEIGHT, width=CANVAS_WIDTH, highlightthickness=0)
