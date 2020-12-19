3d3066f467ee09bda60eacb7b87976b9c81b73b0
john <john@jpatmore.plus.com>
2020-12-19 17:41:10 +0000

    Display English translation

--- angela.py
+++ angela.py
@@ -3,7 +3,7 @@ import pandas
 from random import randint, choice
 
 BACKGROUND_COLOR = "#B1DDC6"
-
+current_card={}
 
 data_frame = pandas.read_csv("data/french_words.csv")
 # print(data_frame)
@@ -13,21 +13,32 @@ to_learn = data_frame.to_dict(orient="records")
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = choice(to_learn)
-    foreign_word = current_card["French"]
-    english_word = current_card["English"]
-    # Change the word on the card
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=foreign_word)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(5000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
 
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="./images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 import pandas
 from random import randint, choice
+import time
 
 
 BACKGROUND_COLOR = "#B1DDC6"
@@ -16,6 +17,10 @@ french_csv = "./data/french_words.csv"
 french_encoding = "uft-8"
 german_csv = "./data/german_words.csv"
 german_encoding = "utf-16"
+foreign_word = ""
+english_word = ""
+language_0 = ""
+language_1 = ""
 
 
 # ---------------- Read Words from file -------------------
@@ -26,42 +31,54 @@ def read_data(data_file, encoding):
         # i.e. The first two words of the file = 0xFFFE
         # NOTE: No need to open the file when using Pandas
         # with open(data_file, mode="r", encoding=encoding) as file:
-        data_frame = pandas.read_csv(data_file)
+        data_frame = pandas.read_csv(data_file, encoding=encoding)
         # print(data_frame)
     except FileNotFoundError:
         messagebox.showinfo(parent=window, title="Error", message=f"The data file {data_file} could not be found.")
     else:
-        # Dictionary comprehension
-        # data_list = [{row.German: row.English} for (index, row) in data_frame.iterrows()]
-        # >>> [{'Ich': 'I'}, {'ist': 'is'}, {'ich': 'I'}, ...]
-
+        global language_0, language_1
         # Pandas .to_dict()
         # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_dict.html
         data_list = data_frame.to_dict(orient="records")
-        # print(data_list)
-        # >>> [{'German': 'Ich', 'English': 'I'}, {'German': 'ist', 'English': 'is'}, ... ]
-
+        language_0 = data_frame.columns[0]
+        language_1 = data_frame.columns[1]
         return data_list
 
 
 def pick_word():
+    global foreign_word, english_word, flip_timer
+    # Cancel event timer if a new word is selected
+    window.after_cancel(flip_timer)
+    # Get a random word
     random_dict = choice(word_list)
-    foreign_word = random_dict["German"]
-    english_word = random_dict["English"]
-    # Change the word on the card
-    canvas_card.itemconfig(canvas_word, text=foreign_word)
+    foreign_word = random_dict[language_0]
+    english_word = random_dict[language_1]
+    # Change the word on the card - German
+    canvas_card.itemconfig(canvas_title, text=language_0, fill="black")
+    canvas_card.itemconfig(canvas_word, text=foreign_word, fill="black")
+    canvas_card.itemconfig(canvas_image, image=image_front)
+    flip_timer = window.after(5000, func=display_card_back)
+
+
+def display_card_back():
+    # Change the word on the card - English
+    canvas_card.itemconfig(canvas_title, text=language_1, fill="white")
+    canvas_card.itemconfig(canvas_word, text=english_word, fill="white")
+    canvas_card.itemconfig(canvas_image, image=image_back)
 
 
 # ---------------- UI Setup -------------------
 
 window = tk.Tk()
-window.title("German Flash Cards")
+window.title("Language Flash Cards")
 window.config(padx=20, pady=20, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(5000, func=display_card_back)
 
 canvas_card = tk.Canvas(bg=BACKGROUND_COLOR, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, highlightthickness=0)
 image_front = tk.PhotoImage(file="./images/card_front.png")
-canvas_card.create_image(424, 287, image=image_front)
+image_back = tk.PhotoImage(file="./images/card_back.png")
+canvas_image = canvas_card.create_image(424, 287, image=image_front)
 canvas_title = canvas_card.create_text(400, 150, text=title, font=title_font)
 canvas_word = canvas_card.create_text(400, 263, text="ABCDEFGHIJKLMNOP", font=word_font)
 
@@ -75,6 +92,7 @@ button_right = tk.Button(image=image_right, relief="flat", borderwidth=0, highli
 canvas_card.grid(row=0, column=0, columnspan=2)
 button_wrong.grid(row=1, column=0)
 button_right.grid(row=1, column=1)
+
 
 # canvas_title.config(text="French")
 # word_list = read_data(french_csv, french_encoding)
